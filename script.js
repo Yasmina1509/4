3a396d1f7b3abf4e8708805dff224c2ee275a6b2
Yasmina1509 <xikmatullaevayasmina115@gmail.com>
2024-05-10 14:25:43 +0500

    5.4

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+let h1 = document.getElementById("random")
+
+let body = document.querySelector("body")
+
+h1.style.backgroundColor = "red"
+body.style.backgroundColor = "black"
+body.style.display = "flex"
+body.style.justifyContent = "center"
+body.style.alignItems = "center"
+body.style.height = "100vh"
+let num = Math.floor(Math.random() * 100)
+h1.textContent = num
+
 
